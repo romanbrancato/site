5b824432a0bf0c8ec5c2e630c561508595d7b468
romanbrancato <romanbrancato88@gmail.com>
2024-12-20 18:39:57 -0500

    initial style and layout design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,67 @@
-import Image from "next/image";
+import {Building2, MapPin} from 'lucide-react'
+import {ScrambleText} from "@/components/scramble-text";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <div className="min-h-screen antialiased font-[family-name:var(--font-geist-mono)]">
+            <main className="max-w-4xl mx-auto px-4 py-8">
+                <nav className="flex items-center justify-between mb-12 text-sm">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                </nav>
+                <header className="mb-16 space-y-4">
+                    <h1 className="text-4xl font-bold mb-4 animate-fade-in text-white">
+                <span className="inline-block">
+                    <ScrambleText text={"roman brancato"} />
+                </span>
+                    </h1>
+                    <div className="flex flex-col gap-2 text-gray-400">
+                        <div className="flex items-center gap-2">
+                            <MapPin size={16}/>
+                            <span>michigan, united states</span>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <Building2 size={16}/>
+                            <span>software developer at claimchoice</span>
+                        </div>
+                    </div>
+                    <p className="leading-relaxed text-[#d1d5db]">
+                        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </header>
+                <section className="mb-16 animate-fade-in-up">
+                    <h2 className="text-2xl font-bold mb-6 flex items-center text-white">
+                        education
+                    </h2>
+                    <div className="space-y-8">
+                        <div className="group">
+                            <div
+                                className="text-xl font-semibold mb-1 text-white group-hover:text-orange-600 transition-colors duration-200">
+                                grand valley state university
+                            </div>
+                            <p className="text-sm text-gray-400 mb-2">
+                                2020 - 2024
+                            </p>
+                            <p className="text-gray-300">
+                                bachelor degree in computer science
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <section className="mb-16 animate-fade-in-up">
+                    <h2 className="text-2xl font-bold mb-6 flex items-center text-white">
+                        work
+                    </h2>
+                </section>
+                <section className="mb-16 animate-fade-in-up">
+                    <h2 className="text-2xl font-bold mb-6 flex items-center text-white">
+                        projects
+                    </h2>
+                </section>
+            </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
