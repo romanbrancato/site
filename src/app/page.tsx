35f33f5ecba589ffc4622c1f722343dabd6f27cb
romanbrancato <romanbrancato88@gmail.com>
2024-12-20 22:53:45 -0500

    fixed deploy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="min-h-screen antialiased font-[family-name:var(--font-geist-mono)]">
             <main className="max-w-4xl mx-auto px-4 py-8">
                 <nav className="flex items-center justify-between mb-12 text-sm">
-
+                    QUADDY GOON
                 </nav>
                 <header className="mb-16 space-y-4">
                     <h1 className="text-4xl font-bold mb-4 animate-fade-in text-white">
@@ -24,7 +24,7 @@ export default function Home() {
                             <span>software developer at claimchoice</span>
                         </div>
                     </div>
-                    <p className="leading-relaxed text-[#d1d5db]">
+                    <p className="leading-relaxed text-[#d1d5db] animate-fade-in">
                         lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
@@ -34,12 +34,13 @@ export default function Home() {
                 </header>
                 <section className="mb-16 animate-fade-in-up">
                     <h2 className="text-2xl font-bold mb-6 flex items-center text-white">
+                        <span className="text-[#AF1740] mr-2">{"&"}</span>
                         education
                     </h2>
                     <div className="space-y-8">
                         <div className="group">
                             <div
-                                className="text-xl font-semibold mb-1 text-white group-hover:text-orange-600 transition-colors duration-200">
+                                className="text-xl font-semibold mb-1 text-white group-hover:text-[#AF1740] transition-colors duration-200">
                                 grand valley state university
                             </div>
                             <p className="text-sm text-gray-400 mb-2">
@@ -53,12 +54,20 @@ export default function Home() {
                 </section>
                 <section className="mb-16 animate-fade-in-up">
                     <h2 className="text-2xl font-bold mb-6 flex items-center text-white">
+                        <span className="text-[#AF1740] mr-2">{"&"}</span>
                         work
                     </h2>
                 </section>
                 <section className="mb-16 animate-fade-in-up">
                     <h2 className="text-2xl font-bold mb-6 flex items-center text-white">
+                        <span className="text-[#AF1740] mr-2">{"&"}</span>
                         projects
+                    </h2>
+                </section>
+                <section className="mb-16 animate-fade-in-up sticky bottom-0">
+                    <h2 className="text-2xl font-bold mb-6 flex items-center text-white">
+                        <span className="text-[#AF1740] mr-2">{"&"}</span>
+                        links
                     </h2>
                 </section>
             </main>
